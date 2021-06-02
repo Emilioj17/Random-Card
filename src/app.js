f2ab09c3725f9720b7a0144a0140ed3c59d519f1
Emilio Jelves <ejjelves@gmail.com>
2021-06-02 15:47:13 -0400

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let centro = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let acortar = document.querySelectorAll(".fas");
+  let figuraDerecha = ["♥", "♠", "♦", "♣"];
+  let figuraIzquierda = ["♥", "♠", "♦", "♣"];
+
+  let centroIndex = Math.floor(Math.random() * centro.length);
+  let figuraIndex = Math.floor(Math.random() * figuraDerecha.length);
+
+  document.querySelector(".figuraCentro").innerHTML = `${centro[centroIndex]}`;
+  document.querySelector(
+    ".figuraDerecha"
+  ).innerHTML = `${figuraDerecha[figuraIndex]}`;
+  document.querySelector(
+    ".figuraIzquierda"
+  ).innerHTML = `${figuraIzquierda[figuraIndex]}`;
+
+  if (figuraDerecha[figuraIndex] == "♥" || figuraDerecha[figuraIndex] == "♠") {
+    document.querySelector(".figuraDerecha").style = "color:red";
+    document.querySelector(".figuraIzquierda").style = "color:red";
+  }
 };
